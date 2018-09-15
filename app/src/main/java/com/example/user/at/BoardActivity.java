d2b90b08a20f0f37d2c737f02f0e0c47f368ee9d
YangYoHan <rewrite1209@daum.net>
2018-09-15 15:11:52 +0900

    게시판 보는 거 완성

--- app/src/main/java/com/example/user/at/BoardActivity.java
+++ app/src/main/java/com/example/user/at/BoardActivity.java
@@ -6,27 +6,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
 public class BoardActivity extends AppCompatActivity {
     Skin skin;
     int color;
-    String[] testTimes={"2018.04.30 14:20","2018.04.28 14:20","2018.04.27 14:20","2018.04.01 14:20","2018.04.01 14:20","2018.04.01 14:20","2018.04.01 14:20","2018.04.01 14:20","2018.04.01 14:20"};
-    String[] testTitles={"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa","bbbb","cccc","abcd","555","2","2","2","2"};
-    String[] testWriters={"Tea","Coffee","Bean","Tom","behind","2","2","2","2"};
-    String[] testfeedbacks={"2","3","2","1","4","2","2","2","2","2"};
-    String[] testrecommends={"2","3","2","1","4","2","2","2","2","2"};
-
-    String categoryName;
     RecyclerView boardRecycler;
     LinearLayoutManager layoutManager;
     ArrayList<MyInfoItem> items;
     MyInfoAdapter adapter;
     int num;
     JsonObjectRequest jsonRequest;
+    String time,title,writer,feedback,recommend;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,16 +40,58 @@ public class BoardActivity extends AppCompatActivity {
         layoutManager=new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        items=new ArrayList();
-        for (num=0;num<=8;num++){
-            items.add(new MyInfoItem(testTimes[num],testTitles[num],testWriters[num],testfeedbacks[num],testrecommends[num]));
+        Response.Listener bListener=new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                Log.d("TAG", "JSONObj response=" + response);
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray=jsonResponse.getJSONArray("sign");
+
+                    items=new ArrayList<>();
+
+                    for(int i=0;i<jsonArray.length();i++){
+                        JSONObject row=jsonArray.getJSONObject(i);
+                        time=row.getString("create_time");
+                        title=row.getString("post_title");
+                        writer=row.getString("member_id");
+                        feedback="0";
+                        recommend=String.valueOf(row.getInt("recommend"));
+                        items.add(new MyInfoItem(time,title,writer,feedback,recommend));
+                    }
+
+                    boardRecycler.setLayoutManager(layoutManager);
+                    boardRecycler.setItemAnimator(new DefaultItemAnimator());
+                    adapter=new MyInfoAdapter(items);
+                    boardRecycler.setAdapter(adapter);
+
+                }catch (Exception e){
+                    Log.d("dberror",e.toString());
+                }
+            }
         };
 
-        boardRecycler.setLayoutManager(layoutManager);
-        boardRecycler.setItemAnimator(new DefaultItemAnimator());
-        adapter=new MyInfoAdapter(items);
-        boardRecycler.setAdapter(adapter);
+        BoardRequest bRequest = new BoardRequest(0,bListener);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(bRequest);
     }
+/*
+    public void processResponse(JSONArray response) {
+        try {
+            for(int i=0;i<response.length();i++){
+                JSONObject row=response.getJSONObject(i);
+                time=row.getString("create_time");
+                title=row.getString("post_title");
+                writer=row.getString("member_id");
+                feedback="0";
+                recommend=String.valueOf(row.getInt("recommend"));
+                items.add(new MyInfoItem(time,title,writer,feedback,recommend));
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }*/
 
     @Override
     public void onBackPressed() {
